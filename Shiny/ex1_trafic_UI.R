159024783174f220fad637bb617ccf94d22e47c1
Fernando-Emanuel Donea <fernandodonea10@gmail.com>
2026-01-30 11:44:55 +0200

    small UI update for ex1

--- Shiny/ex1_trafic_UI.R
+++ Shiny/ex1_trafic_UI.R
@@ -4,17 +4,13 @@ ex1_trafic_UI <- function(id) {
   ns <- NS(id)
   library(plotly) # pentru a putea viziona histogramele
 
-  tagList
-  (
-    sidebarLayout
-    (
-      sidebarPanel
-      (
+  tagList(
+    sidebarLayout(
+      sidebarPanel(
         h4("Parametrii Trafic"),
 
         # algerea intre Poisson si Binomiala
-        selectInput
-        (
+        selectInput(
           ns("distributie_trafic"), "Distributie Trafic (K_d)",
           choices = c("Poisson", "Binomiala")
         ),
@@ -30,26 +26,37 @@ ex1_trafic_UI <- function(id) {
         numericInput(ns("sim_ani"), "Ani simulare", value = 3, min = 1),
         actionButton(ns("btn_sim_trafic"), "Simuleaza Trafic", class = "btn-primary")
       ),
-      mainPanel
-      (
-        tabPanel(
-          "Histograme",
-          plotlyOutput(ns("plot_trafic_anual")),
-          uiOutput(ns("ui_reset_button")),
-          plotlyOutput(ns("plot_trafic_lunar"))
-        ),
-        tabPanel(
-          "Statistici si Analiza",
-          tableOutput(ns("statistici_trafic_anual")),
-          hr(),
-          h3("Interpretare Rezultate"),
-          p(strong("1. Comparatie Empiric vs Teoretic:")),
-          p("Se observa ca pe masura ce dimensiunea esantionului creste (mai multi ani), media si varianta empirica converg catre valorile teoretice"),
-          p(strong("2. Diferente intre Modele (Trafic Redus vs Plafonat):")),
-          p("• ", strong("Modelul Poisson"), " este adecvat pentru trafic redus, unde sosirile sunt rare si independente. Varianta este egala cu media."),
-          p("• ", strong("Modelul Binomial"), " este adecvat pentru trafic plafonat (ex. capacitate finita), unde numarul de clienti nu poate depasi o valoare n. Varianta este mai mica decat media (sub-dispersie), ceea ce indica o variabilitate mai redusa datorita plafonarii."),
-          br(),
-          br()
+      mainPanel(
+        tabsetPanel(
+          tabPanel(
+            "Histograme",
+            br(),
+            h4("Histograme Trafic"),
+            plotlyOutput(ns("plot_trafic_anual")),
+            hr(),
+            uiOutput(ns("ui_reset_button")),
+            plotlyOutput(ns("plot_trafic_lunar")),
+            br(),
+            hr(),
+            h4("Interpretare Comparativa"),
+            uiOutput(ns("interpretare_comparativa"))
+          ),
+          tabPanel(
+            "Estimari",
+            br(),
+            h4("Estimare medie si varianta (empiric vs teoretic)"),
+            tableOutput(ns("statistici_trafic_anual"))
+          ),
+          tabPanel(
+            "Explicatii",
+            br(),
+            h4("Interpretare rezultate si comparatie modele"),
+            p(strong("1. Comparatie empiric vs teoretic:")),
+            p("Se observa ca pe masura ce dimensiunea esantionului creste (mai multi ani), media si varianta empirica converg catre valorile teoretice"),
+            p(strong("2. Diferente intre modele (Trafic redus vs plafonat):")),
+            p("• ", strong("Modelul Poisson"), " este adecvat pentru trafic redus, unde sosirile sunt rare si independente. Varianta este egala cu media."),
+            p("• ", strong("Modelul Binomial"), " este adecvat pentru trafic plafonat (ex. capacitate finita), unde numarul de clienti nu poate depasi o valoare n. Varianta este mai mica decat media (sub-dispersie), ceea ce indica o variabilitate mai redusa datorita plafonarii.")
+          )
         )
       )
     )
